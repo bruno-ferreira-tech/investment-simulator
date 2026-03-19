ba0d7c1f30e389b4230982c6816fa0c0821d07b1
Bruno Ferreira <brunoferreiraj3@gmail.com>
2026-03-18 22:44:21 -0300

    chore: configure tsconfig for nodejs backend

---
+++ src/domain/value-objects/InterestRate.ts
@@ -0,0 +1,22 @@
+export class InterestRate {
+
+    private readonly value: number;
+
+    //Recebe a taxa em percentual (ex: 12 para 12%) e armazena como decimal (ex: 0.12)
+    constructor(taxa: number){
+
+        if (taxa < 0) {
+            throw new Error("Valor não pode ser menor que zero!")
+        }
+
+        if (taxa > 100) {
+            throw new Error("Valor não pode ser maior que 100")
+        }
+
+        this.value = taxa / 100
+    }
+
+    getValue (){
+        return this.value
+    }
+}
