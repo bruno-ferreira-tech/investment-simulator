2800b76c98f708a41835ec7c3e63ef9eaa5f72f6
Bruno Ferreira <brunoferreiraj3@gmail.com>
2026-03-22 21:08:36 -0300

    test(simulation): add unit tests for Simulation and Value Objects

--- src/domain/value-objects/InterestRate.ts
+++ src/domain/value-objects/InterestRate.ts
@@ -5,7 +5,7 @@ export class InterestRate {
     //Recebe a taxa em percentual (ex: 12 para 12%) e armazena como decimal (ex: 0.12)
     constructor(taxa: number){
         if (taxa < 0) {
-            throw new Error("Valor não pode ser menor que zero!")
+            throw new Error("Taxa não pode ser negativa")
         }
 
         if (taxa > 100) {
